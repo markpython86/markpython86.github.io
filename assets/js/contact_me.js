0b6bac8eb4219aa2077cb77258c9a1e92d117f12
Mark <37888979+markpython86@users.noreply.github.com>
2018-12-09 15:33:34 -0600

    update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "../mail/contact_me.php",
+        url: "././mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
@@ -32,7 +32,7 @@ $(function() {
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          $('#success >.alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
             .append("<strong>Your message has been sent. </strong>");

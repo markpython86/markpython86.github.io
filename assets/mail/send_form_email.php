202f2eaa3600f563521edfa0b51af23bda0a58b0
Mark <mark.faraj86@gmail.com>
2018-12-09 19:09:23 -0600

    testing the emai form

--- assets/mail/send_form_email.php
+++ assets/mail/send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "mark.python86@gmail.com";
+    $email_to = "mark.faraj86@gmail.com";
     $email_subject = "Your email subject line";
  
     function died($error) {
@@ -16,39 +16,35 @@ if(isset($_POST['email'])) {
  
  
     // validation expected data exists
-    if(!isset($_POST['name']) ||
+    if(!isset($_POST['first_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['phone']) ||
-        !isset($_POST['message'])) {
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
  
-    $name = $_POST['name']; // required
-    $email_address = $_POST['email']; // required
-    $phone = $_POST['phone']; // not required
-    $message = $_POST['message']; // required
+    $first_name = $_POST['first_name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
-  if(!preg_match($email_exp,$email_address)) {
+  if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$name)) {
+  if(!preg_match($string_exp,$first_name)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($message) < 2) {
-    $error_message .= 'The message you entered do not appear to be valid.<br />';
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -65,14 +61,14 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_address)."\n";
-    $email_message .= "phone: ".clean_string($phone)."\n";
-    $email_message .= "message: ".clean_string($message)."\n";
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
  
 // create email headers
-$headers = 'From: '.$email_address."\r\n".
-'Reply-To: '.$email_address."\r\n" .
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>

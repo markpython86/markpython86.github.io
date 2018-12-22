7a71abecf67235c500c940440b3b012457d7f9a0
Mark <mark.faraj86@gmail.com>
2018-12-22 15:16:03 -0600

    adding the firebase key

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyBZRdcDcwmrAyN1dxnPVQ1DN3cu5ulf5c4',
+    authDomain: "portfolio-ccfdd.firebaseapp.com",
+    databaseURL: "https://portfolio-ccfdd.firebaseio.com",
+    projectId: "portfolio-ccfdd",
+    storageBucket: "",
+    messagingSenderId: "104875280453"
+  };
+  
